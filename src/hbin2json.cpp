e29627c7a0738b8570ff03963beaf833dd7b57d4
Sergey Chaban <sergey.chaban@gmail.com>
2023-03-22 13:15:31 +0200

    if input is .json dump raw tokens;

--- src/hbin2json.cpp
+++ src/hbin2json.cpp
@@ -27,6 +27,53 @@ void hbin_str_out(FILE* pOut, HBIN_STRING str) {
 	}
 }
 
+static void fmtU64(char* pBuf, size_t bsize, uint64_t val) {
+	uint32_t* p = (uint32_t*)&val;
+	uint32_t lo = p[0];
+	uint32_t hi = p[1];
+	XD_SPRINTF(XD_SPRINTF_BUF(pBuf, bsize), "%08X%08X", hi, lo);
+}
+
+class TokEcho : public cxLexer::TokenFunc {
+public:
+virtual bool operator()(const cxLexer::Token& tok) {
+	char str64[32];
+	if (tok.id == cxLexer::TokId::TOK_FLOAT) {
+		nxCore::dbg_msg("float %.12f @ (%d, %d)\n", tok.val.f, tok.loc.line, tok.loc.column);
+	} else if (tok.id == cxLexer::TokId::TOK_INT) {
+		fmtU64(str64, sizeof(str64), tok.val.i);
+		nxCore::dbg_msg("int 0x%s @ (%d, %d)\n", str64, tok.loc.line, tok.loc.column);
+	} else if (tok.is_punctuation()) {
+		nxCore::dbg_msg("punct %s @ (%d, %d)\n", tok.val.c, tok.loc.line, tok.loc.column);
+	} else if (tok.id == cxLexer::TokId::TOK_QSTR) {
+		nxCore::dbg_msg("\"%s\" @ (%d, %d)\n", (char*)tok.val.p, tok.loc.line, tok.loc.column);
+	} else if (tok.id == cxLexer::TokId::TOK_SQSTR) {
+		nxCore::dbg_msg("'%s' @ (%d, %d)\n", (char*)tok.val.p, tok.loc.line, tok.loc.column);
+	} else if (tok.is_symbol()) {
+		nxCore::dbg_msg("symbol %s @ (%d, %d)\n", (char*)tok.val.p, tok.loc.line, tok.loc.column);
+	} else if (tok.is_keyword()) {
+		nxCore::dbg_msg("keyword %s @ (%d, %d)\n", (char*)tok.val.p, tok.loc.line, tok.loc.column);
+	} else {
+		nxCore::dbg_msg("unknown @ (%d, %d)\n", tok.loc.line, tok.loc.column);
+	}
+	return true;
+}
+};
+
+static void json_tokenize(const char* pSrcPath) {
+	if (!pSrcPath) return;
+	size_t srcSize = 0;
+	char* pSrc = (char*)nxCore::raw_bin_load(pSrcPath, &srcSize);
+	if (!pSrc) {
+		nxCore::dbg_msg("json tokenize: unable to load \"%s\"\n", pSrcPath);
+		return;
+	}
+	cxLexer lex;
+	lex.set_text(pSrc, srcSize);
+	TokEcho echo;
+	lex.scan(echo);
+}
+
 
 int main(int argc, char* argv[]) {
 	nxApp::init_params(argc, argv);
@@ -40,6 +87,8 @@ int main(int argc, char* argv[]) {
 			cvt_bgeo(pSrcPath);
 		} else if (nxCore::str_ends_with(pSrcPath, ".bclip")) {
 			cvt_bclip(pSrcPath);
+		} else if (nxCore::str_ends_with(pSrcPath, ".json")) {
+			json_tokenize(pSrcPath);
 		}
 	}
 
