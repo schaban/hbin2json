4c9f74cab262d5aca2ca79c66d6bc0f6015ca4c1
Sergey Chaban <sergey.chaban@gmail.com>
2023-08-06 11:21:15 +0300

    + SH eval functions;

--- smp/webgl/scene_utils.js
+++ smp/webgl/scene_utils.js
@@ -504,6 +504,105 @@ function triNormalCW(v0, v1, v2) {
 }
 
 
+function shEval2(sh, x, y, z) {
+	let zz = z*z;
+	let s0 = y, c0 = x;
+	sh[0] = 0.282094791774;
+	sh[2] = 0.488602511903 * z;
+	const tmp = -0.488602511903;
+	sh[1] = tmp * s0;
+	sh[3] = tmp * c0;
+}
+
+function shEval3(sh, x, y, z) {
+	let zz = z*z;
+	let tmp, prev0, prev1, prev2, s0 = y, s1, c0 = x, c1;
+	sh[0] = 0.282094791774;
+	sh[2] = 0.488602511903 * z;
+	sh[6] = 0.946174695758 * zz + -0.315391565253;
+	tmp = -0.488602511903;
+	sh[1] = tmp * s0;
+	sh[3] = tmp * c0;
+	prev1 = -1.09254843059 * z;
+	sh[5] = prev1 * s0;
+	sh[7] = prev1 * c0;
+	s1 = x*s0 + y*c0;
+	c1 = x*c0 - y*s0;
+	tmp = 0.546274215296;
+	sh[4] = tmp * s1;
+	sh[8] = tmp * c1;
+}
+
+function shEval6(sh, x, y, z) {
+	let zz = z*z;
+	let tmp, prev0, prev1, prev2, s0 = y, s1, c0 = x, c1;
+	sh[0] = 0.282094791774;
+	sh[2] = 0.488602511903 * z;
+	sh[6] = 0.946174695758 * zz + -0.315391565253;
+	sh[12] =  ( 1.86588166295 * zz + -1.11952899777 )  * z;
+	sh[20] = 1.9843134833 * z * sh[12] + -1.00623058987 * sh[6];
+	sh[30] = 1.98997487421 * z * sh[20] + -1.00285307284 * sh[12];
+	tmp = -0.488602511903;
+	sh[1] = tmp * s0;
+	sh[3] = tmp * c0;
+	prev1 = -1.09254843059 * z;
+	sh[5] = prev1 * s0;
+	sh[7] = prev1 * c0;
+	prev2 = -2.28522899732 * zz + 0.457045799464;
+	sh[11] = prev2 * s0;
+	sh[13] = prev2 * c0;
+	tmp = -4.6833258049 * zz + 2.00713963067;
+	prev0 = tmp * z;
+	sh[19] = prev0 * s0;
+	sh[21] = prev0 * c0;
+	tmp = 2.03100960116 * z;
+	prev1 = tmp * prev0;
+	tmp = -0.991031208965 * prev2;
+	prev1 += tmp;
+	sh[29] = prev1 * s0;
+	sh[31] = prev1 * c0;
+	s1 = x*s0 + y*c0;
+	c1 = x*c0 - y*s0;
+	tmp = 0.546274215296;
+	sh[4] = tmp * s1;
+	sh[8] = tmp * c1;
+	prev1 = 1.44530572132 * z;
+	sh[10] = prev1 * s1;
+	sh[14] = prev1 * c1;
+	prev2 = 3.31161143515 * zz + -0.473087347879;
+	sh[18] = prev2 * s1;
+	sh[22] = prev2 * c1;
+	tmp = 7.19030517746 * zz + -2.39676839249;
+	prev0 = tmp * z;
+	sh[28] = prev0 * s1;
+	sh[32] = prev0 * c1;
+	s0 = x*s1 + y*c1;
+	c0 = x*c1 - y*s1;
+	tmp = -0.590043589927;
+	sh[9] = tmp * s0;
+	sh[15] = tmp * c0;
+	prev1 = -1.77013076978 * z;
+	sh[17] = prev1 * s0;
+	sh[23] = prev1 * c0;
+	prev2 = -4.40314469492 * zz + 0.489238299435;
+	sh[27] = prev2 * s0;
+	sh[33] = prev2 * c0;
+	s1 = x*s0 + y*c0;
+	c1 = x*c0 - y*s0;
+	tmp = 0.625835735449;
+	sh[16] = tmp * s1;
+	sh[24] = tmp * c1;
+	prev1 = 2.07566231488 * z;
+	sh[26] = prev1 * s1;
+	sh[34] = prev1 * c1;
+	s0 = x*s1 + y*c1;
+	c0 = x*c1 - y*s1;
+	tmp = -0.65638205684;
+	sh[25] = tmp * s0;
+	sh[35] = tmp * c0;
+}
+
+
 function hex(x) {
 	return x.toString(16);
 }
@@ -796,3 +895,4 @@ class Scene {
 }
 
 const scene = new Scene();
+
